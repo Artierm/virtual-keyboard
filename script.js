d47da10b4d20f08e90b5648129bdce3a0a85562c
Artierm <artyom.rus.ct100@mail.ru>
2020-04-13 22:38:54 +0300

    fix:delete comment

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 class KeyBoard {
+
   Element = {
       areaKeyBoard: null,
       KeysBoard: null,
@@ -21,40 +22,40 @@ class KeyBoard {
 
 
   KeysValuesRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-      'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
-      'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
-      'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift','↑',
-      'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '←', '↓', '→'];
+    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
+    'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+    'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift','↑',
+    'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '←', '↓', '→'];
 
 
   KeysEventCode = [
-      'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6',
-       'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
-      'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO',
-     'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
-      'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH',
-     'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
-      'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB',
-     'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp',
-      'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight',
-  'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+    'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6',
+    'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO',
+    'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+    'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH',
+    'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+    'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB',
+    'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp',
+    'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight',
+    'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
 
   SystemKeys = ['Backspace', 'Tab', 'CapsLock', 'Shift', 'MetaLeft', 'AltRight',
-  'AltLeft', 'Space', 'ControlLeft','ControlRight','ShiftLeft','ShiftRight'];
-  ShiftKeysCode = ['Backquote','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
-  'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'BracketLeft',
-  'BracketRight', 'Backslash', 'Semicolon', 'Quote', 'Comma', 'Period', 'Slash','AltRight'];
-  ShiftKeysValue = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
-  '{', '}', '|', ':', '"', '<', '>', '?' , 'Alt'];
-  ShiftKeysCode2 = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6',
-  'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backslash', 'Slash' , 'AltRight'];
-  ShiftKeysValue2 = ['!', '"', '№', ';', '%', ':', '?',
-  '*', '(', ')', '_', '+', '|', ',','Alt'];
-  ShiftKeysNormalValue = ['`','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
-  '[', ']', '\\', ';', '\'', ',', '.', '/','Alt'];
-  ShiftKeysNormalValue2 = ['1', '2', '3', '4', '5',
-  '6', '7', '8', '9', '0', '-', '=','\\','.','Alt'];
+    'AltLeft', 'Space', 'ControlLeft','ControlRight','ShiftLeft','ShiftRight'];
+    ShiftKeysCode = ['Backquote','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
+    'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'BracketLeft',
+    'BracketRight', 'Backslash', 'Semicolon', 'Quote', 'Comma', 'Period', 'Slash','AltRight'];
+    ShiftKeysValue = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+    '{', '}', '|', ':', '"', '<', '>', '?' , 'Alt'];
+    ShiftKeysCode2 = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6',
+    'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backslash', 'Slash' , 'AltRight'];
+    ShiftKeysValue2 = ['!', '"', '№', ';', '%', ':', '?',
+    '*', '(', ')', '_', '+', '|', ',','Alt'];
+    ShiftKeysNormalValue = ['`','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    '[', ']', '\\', ';', '\'', ',', '.', '/','Alt'];
+    ShiftKeysNormalValue2 = ['1', '2', '3', '4', '5',
+    '6', '7', '8', '9', '0', '-', '=','\\','.','Alt'];
 
 
   constructor() {
@@ -64,15 +65,10 @@ class KeyBoard {
 
   startProgram() {
     this.mouseClick();
-    // this.keyDown();
-    // this.keyUp();
     this.mouseShiftLeftDown();
     this.mouseShiftLeftUp();
-    // this.keyShiftLeftDown();
     this.languageValue();
-    // this.keyShiftRightDown();
     this.capsMouseDown();
-    // this.keyUpButton();
     this.mouseShiftRightDown();
     this.mouseShiftRightUp();
   }
@@ -213,18 +209,12 @@ class KeyBoard {
 
 
   static keyDown() {
-    // let buttons = document.querySelectorAll(".button");
-    // for (let i = 0; i < buttons.length; i += 1) {
-    //   buttons[i].setAttribute("id", `${this.KeysEventCode[i]}`);
-    // }
-
     window.addEventListener("keydown", (event) => {
       document.querySelectorAll(".button").forEach((elem) => {
         if (event.code === elem.getAttribute("id")) {
           elem.classList.add("active");
           elem.click();
           event.preventDefault();
-          // this.Element.textarea.focus();
         }
       });
     });
